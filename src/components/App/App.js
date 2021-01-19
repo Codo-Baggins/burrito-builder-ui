06944b9539ad71f7a15708a520399d99a90cf3eb
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-19 09:29:16 -0700

    Fix state of App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
-import {getOrders} from '../../apiCalls';
-import Orders from '../../components/Orders/Orders';
-import OrderForm from '../../components/OrderForm/OrderForm';
+import React, { Component } from "react";
+import "./App.css";
+import { getOrders } from "../../apiCalls";
+import Orders from "../../components/Orders/Orders";
+import OrderForm from "../../components/OrderForm/OrderForm";
 
 class App extends Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = {
+      orders: [],
+    };
   }
 
   componentDidMount() {
-    getOrders()
-      .catch(err => console.error('Error fetching:', err));
+    getOrders().catch((err) => console.error("Error fetching:", err));
   }
 
   render() {
     return (
-      <main className="App">
+      <main className='App'>
         <header>
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} />
       </main>
     );
   }
 }
-
 
 export default App;
